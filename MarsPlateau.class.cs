3316c5e935962ad925a49069e6a5c65bdfda19c7
Paulo A S Lopes <pauloandreh.ti@hotmail.com>
2023-02-03 20:56:13 +0000

    C Sharp code style

--- MarsPlateau.class.cs
+++ MarsPlateau.class.cs
@@ -1,37 +1,37 @@
 
 public class MarsPlateau
 {
-    private string size;
-    private int maxLatitude;
-    private int maxLongitude;
+    private string _size;
+    private int _maxLatitude;
+    private int _maxLongitude;
 
     public MarsPlateau(string size)
     {
-        this.size = size.ToLower();
-        string[] splitedSize = this.size.Split('x');
+        this._size = size.ToLower();
+        string[] splitedSize = this._size.Split('x');
 
-        this.maxLatitude = Int32.Parse(splitedSize[0]);
-        this.maxLongitude = Int32.Parse(splitedSize[1]);
+        this._maxLatitude = Int32.Parse(splitedSize[0]);
+        this._maxLongitude = Int32.Parse(splitedSize[1]);
     }
 
     public String getSize()
     {
-        return this.size;
+        return this._size;
     }
 
     public Int32 getMaxLatitude()
     {
-        return this.maxLatitude;
+        return this._maxLatitude;
     }
 
     public Int32 getMaxLongitude()
     {
-        return this.maxLongitude;
+        return this._maxLongitude;
     }
 
     public bool validatePosition(int latitude, int longitude)
     {
-        if (latitude > this.maxLatitude || longitude > this.maxLongitude)
+        if (latitude > this._maxLatitude || longitude > this._maxLongitude)
         {
             return false;
         }
